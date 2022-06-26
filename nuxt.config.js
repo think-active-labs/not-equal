0a998b914cb397887b62d02a4ad2f412768c2a3a
Andy Garbett <andy@thinkactivelabs.co.uk>
2022-06-26 13:17:46 +0100

    Updating nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  router: {
+    base: '/not-equal/',
+  },
   target: 'static',
-  ssr: true,
   css: ['@/assets/css/tailwind.css'],
   modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
   build: {},
