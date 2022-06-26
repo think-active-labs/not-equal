dd0fa569cd6bf25805b27e167824638d3e07017c
Andy Garbett <andy@thinkactivelabs.co.uk>
2022-06-26 13:39:45 +0100

    Updating CI

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,9 +2,9 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  // app: {
-  //   baseURL: '/not-equal/',
-  // },
+  app: {
+    baseURL: '/not-equal/',
+  },
   target: 'static',
   css: ['@/assets/css/tailwind.css'],
   modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
