667578ae5f2b8be40309e5ede5d5666f20a9c058
Andy Garbett <andy@thinkactivelabs.co.uk>
2022-06-26 12:57:31 +0100

    Updated CI config

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   target: 'static',
-  ssr: false,
+  ssr: true,
   css: ['@/assets/css/tailwind.css'],
   modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
   build: {},
