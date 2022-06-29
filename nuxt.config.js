9b6473d42009b3114a34518b3f8f3d40a6f8dbce
Andy Garbett <andy@thinkactivelabs.co.uk>
2022-06-29 08:38:46 +0100

    Updating nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,9 +3,9 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   // ssr: false,
-  // generate: {
-  //   fallback: true,
-  // },
+  generate: {
+    fallback: true,
+  },
   router: {
     base: '/not-equal/',
   },
