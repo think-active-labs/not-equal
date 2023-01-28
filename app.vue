5bc078369ff17be367eb30e6e77a2949e8a50ceb
Andy Garbett <andy@thinkactivelabs.co.uk>
2023-01-28 17:10:24 +0000

    Updates

--- app.vue
+++ app.vue
@@ -4,10 +4,4 @@
   </div>
 </template>
 <script setup>
-import cms from 'netlify-cms-app'
-import { Widget as FileRelationWidget } from '@ncwidgets/file-relation'
-
-cms.registerWidget(FileRelationWidget)
-
-cms.init()
 </script>
