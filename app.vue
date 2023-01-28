b75b1daa94b36c62f6306ed1e3a84c32e5e01c0f
Andy Garbett <andy@thinkactivelabs.co.uk>
2023-01-28 17:07:28 +0000

    Adding custom widget

--- app.vue
+++ app.vue
@@ -3,3 +3,11 @@
     <NuxtPage />
   </div>
 </template>
+<script setup>
+import cms from 'netlify-cms-app'
+import { Widget as FileRelationWidget } from '@ncwidgets/file-relation'
+
+cms.registerWidget(FileRelationWidget)
+
+cms.init()
+</script>
