b401b985c3ed667bff138bb3e68b54ac470b89af
Andy Garbett <andy@thinkactivelabs.co.uk>
2023-02-06 22:43:28 +0000

    Updating image block

--- components/ImageGallery.vue
+++ components/ImageGallery.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="flex gap-3 overflow-auto flex-wrap justify-center">
-    <img v-for="i, index in images" :key="index" class="object-cover w-full aspect-square sm:w-1/4 max-h-52 lg:max-h-80"
-      :src="i" />
+    <div v-for="i, index in section.images" :key="index">
+      <img class="object-cover w-full aspect-square sm:w-1/4 max-h-52 lg:max-h-80" :src="i.image" />
+      <p>{{ i.caption }}</p>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  components: {
-  },
-  props: ['images'],
-  mounted() {
-  }
-};
+<script setup lang="ts">
+
+interface ImageBlock {
+  type: string,
+  images: [{
+    image: string,
+    caption: string
+  }],
+}
+
+const props = defineProps<{
+  section: ImageBlock
+}>()
 </script>
