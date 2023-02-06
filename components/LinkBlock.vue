a3c7de3d9a2203fbfacc304dfb22e13b1a67dd6d
Andy Garbett <andy@thinkactivelabs.co.uk>
2023-02-06 08:47:43 +0000

    Adding linkblock component

--- components/LinkBlock.vue
+++ components/LinkBlock.vue
@@ -1,23 +1,33 @@
 <template>
-  <div class="xl:w-1/2 prose-xl m-auto">
-    <pre>
-      {{ section }}
-    </pre>
+  <div class="prose-xl m-auto">
     <h2>{{ section.title }}</h2>
-    <div v-for="link in section.links" class="py-1">
-      <div v-html="$md.render(link.content)"></div>
+    <div v-for="button in section.buttons" class="py-1">
+      <div v-html="parseMarkdown(button.content)" />
       <div class="flex flex-col sm:flex-row gap-4">
-        <a v-for="button in link.buttons" :href="button.url" target="_blank"
-          class="py-2 px-4 border-solid border-4 border-gray-600 text-2xl font-semibold hover:bg-gray-600 hover:text-white">{{
-  button.text
-          }}</a>
+        <a :href="button.url" target="_blank"
+          class="py-2 px-4 border-solid border-4 border-gray-600 text-2xl font-semibold hover:bg-gray-600 hover:text-white">
+          {{ button.text }}
+        </a>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  props: ['section'],
+<script setup lang="ts">
+import { micromark } from 'micromark'
+
+interface LinkBlock {
+  type: string,
+  title: string,
+  content: string,
+  buttons: [{ text: string, url: string, content: string }],
+}
+
+const props = defineProps<{
+  section: LinkBlock
+}>()
+
+const parseMarkdown = (s: string) => {
+  return micromark(s)
 }
 </script>
