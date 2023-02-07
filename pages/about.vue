ffa1012b6a52445911cac198f0de4fc94afd13ca
Andy Garbett <andy@thinkactivelabs.co.uk>
2023-02-07 22:51:24 +0000

    Adding padding to about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="w-full mb-20">
-    <NuxtLink :to="`/`" class="absolute ml-5 mt-5 invisible sm:visible text-2xl font-semibold cursor-pointer">←
+    <NuxtLink :to="`/`" class="absolute ml-5 mt-5 text-2xl font-semibold cursor-pointer">←
       <span class="hover:underline">Back</span>
     </NuxtLink>
     <h1 class="font-title text-8xl pt-24 pb-12 text-center">Not-Equal</h1>
     <div class="w-full border-b-4 border-black p-12">
       <img v-if="data?.image" :src="data?.image" class="object-contain m-auto max-h-[50vh]" :alt="data?.description" />
     </div>
-    <ContentDoc class="prose sm:prose-xl m-auto pt-20" />
+    <ContentDoc class="prose sm:prose-xl m-auto pt-20 py-3" />
   </div>
 </template>
 
