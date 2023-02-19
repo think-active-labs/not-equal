e8b9a23d461692352c64e51f23e4b0decd12ea48
Andy Garbett <andy@thinkactivelabs.co.uk>
2023-02-19 17:19:54 +0000

    Fix Text wrapping on the titles on Theme pages

--- pages/[theme]/index.vue
+++ pages/[theme]/index.vue
@@ -2,16 +2,16 @@
   <div class="w-full">
     <div class="h-full grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4" v-if="data?.theme">
       <div class="p-3 sm:p-6 border-r-2 border-b-2 border-black">
-        <h1 class="font-title text-3xl sm:text-4xl md:text-5xl mb-4 antialiased">{{ data?.theme.title }}</h1>
-        <p class="font-subtitle text-lg sm:text-xl md:text-3xl">{{ data?.theme.description }}</p>
+        <h1 class="font-title text-3xl mb-4 antialiased">{{ data?.theme.title }}</h1>
+        <p class="font-subtitle text-lg sm:text-2xl">{{ data?.theme.description }}</p>
       </div>
       <div v-for="p, index in data?.projects"
         class="flex flex-col gap-2 p-3 sm:p-6 border-r-2 border-b-2 border-black justify-start">
         <nuxt-link :to="`${theme}/${p._path!.replace('/projects/', '')}`">
-          <img class="object-contain" :src="p.image" />
+          <img class="object-cover w-full" :src="p.image" />
         </nuxt-link>
         <div class="flex flex-col gap-4">
-          <h1 class="font-title text-xl sm:text-5xl antialiased">{{ p.title }}</h1>
+          <h1 class="font-title text-2xl antialiased">{{ p.title }}</h1>
           <p class="font-subtitle text-base md:text-xl text-clip">{{ p.description }}</p>
         </div>
         <nuxt-link :to="`${theme}/${p._path!.replace('/projects/', '')}`"
@@ -21,23 +21,6 @@
         <h1 class="font-subtitle sm:text-4xl md:text-7xl mb-4">Next Chapter</h1>
       </div>
     </div>
-    <!-- <div class="h-full w-full flex flex-wrap">
-      <div class="w-full sm:h-1/2 sm:w-1/3 lg:w-1/4 p-6 border-r-2 border-b-2 border-black">
-        <h1 class="font-title text-5xl mb-4">{{ data?.theme.title }}</h1>
-        <p class="font-subtitle text-lg sm:text-xl md:text-3xl">{{ data?.theme.description }}</p>
-      </div>
-
-      <div v-for="p, index in data?.projects"
-        class="w-full sm:h-1/2 sm:w-1/3 lg:w-1/4 flex flex-col gap-2 p-6 border-r-2 border-b-2 border-black justify-between overflow-hidden">
-        <img class="object-contain" :src="p.image" />
-        <h1 class="font-title text-4xl">{{ p.title }}</h1>
-        <p class="font-subtitle text-base md:text-xl text-clip">{{ p.description }}</p>
-        <nuxt-link :to="`${theme}/${p._path!.replace('/projects/', '')}`" class="text-xl sm:text-4xl">→</nuxt-link>
-      </div>
-      <div class="sm:h-1/2 sm:w-1/3 lg:w-1/4 p-6 border-r-2 border-b-2 border-black">
-        <h1 class="font-subtitle sm:text-4xl md:text-7xl mb-4">Next Chapter</h1>
-      </div>
-    </div> -->
   </div>
 </template>
 
