d822bec27b3e2d0325712b50bebf6b1d4cdaa10d
Andy Garbett <andy@thinkactivelabs.co.uk>
2023-02-25 09:42:56 +0000

    Updating error page

--- error.vue
+++ error.vue
@@ -2,7 +2,7 @@
   <div class="flex h-full min-h-screen">
     <div class="flex flex-col w-full m-auto text-center">
       <p class="text-8xl antialiased leading-10">{{ props.error?.statusCode }}</p>
-      <p class="text-2xl">{{ props.error?.statusMessage }}</p>
+      <p class="text-3xl">{{ props.error?.statusMessage }}</p>
       <NuxtLink to="/" class="hover:underline">Go home</NuxtLink>
     </div>
   </div>
