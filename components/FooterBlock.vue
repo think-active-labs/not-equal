74d8e96f4a82b294c16158417a47b537c6dbfa53
Andy Garbett <andy@thinkactivelabs.co.uk>
2023-03-22 09:23:18 +0000

    Updating footer image max height

--- components/FooterBlock.vue
+++ components/FooterBlock.vue
@@ -2,8 +2,8 @@
   <div v-if="config">
     <div class="bg-zinc-800 py-4 px-3 sm:py-8 sm:px-12">
       <div class="flex gap-1">
-        <nuxt-img :src="i.image" format="webp" sizes="sm:100vw md:50vw lg:400px" quality="60" class="w-1/2 sm:w-1/5"
-          v-for="i in config['footer-images'].images" />
+        <nuxt-img :src="i.image" format="webp" sizes="sm:100vw md:50vw lg:400px" quality="60"
+          class="w-1/2 sm:w-1/5 max-h-20 object-contain" v-for="i in config['footer-images'].images" />
       </div>
     </div>
     <div class="bg-zinc-900 py-4 px-3 sm:py-3 sm:px-12 text-center sm:text-left">
@@ -18,6 +18,4 @@ const today = new Date().getFullYear()
 const config = SiteConfig
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
